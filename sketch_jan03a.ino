2a45a3b49ab98c1a32b71feadb84a07fc025d33d
Juerd Waalboer <juerd@tnx.nl>
2020-01-04 17:34:50 +0100

    ja

--- sketch_jan03a.ino
+++ sketch_jan03a.ino
@@ -1,37 +1,117 @@
-int p = 25;
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <esp_task_wdt.h>
 
-uint8_t row[] = { 34, 35, 32, 33, 25 };    // +
-uint8_t col[] = { /* groen */ 4, 17, 18, /* rood */ 16, 5, 19 };  // n fet
+const char* ssid = "revspace-pub-2.4ghz";
+const char* password = "";
+const char* mqtt_server = "hoera10jaar.revspace.nl";
 
-void testmatrix(int ms) {
-  for (int c = 0; c < sizeof(col); c++) {
-    digitalWrite(col[c], HIGH);
-    for (int r = 0; r < sizeof(row); r++) {
-      if (ms) Serial.printf("%d, %d (IO %d, %d)\n", r, c, row[r], col[c]);
-      digitalWrite(row[r], HIGH);
-      delay(ms);
-      digitalWrite(row[r], LOW);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+uint8_t row[] = { 27, 26, 32, 33, 25 };    // +
+uint8_t col[] = { /* rood */ 4, 17, 18, /* groen */ 16, 5, 19 };  // n fet
+int leds[30];
+float brightness = .1;
+
+void setup_wifi() {
+  Serial.printf("Connecting to %s\n", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());  // ?
+  Serial.printf("\nIP address: %s\n", WiFi.localIP().toString().c_str());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  int lednr = -1;
+
+  while (*topic != '/') topic++;
+  topic++;
+
+  if (     strcmp(topic, "leeuwarden") == 0) lednr = 0;
+  else if (strcmp(topic, "amsterdam")  == 0) lednr = 1;
+  else if (strcmp(topic, "utrecht")    == 0) lednr = 2;
+  else if (strcmp(topic, "denhaag")    == 0) lednr = 3;
+  else if (strcmp(topic, "rotterdam")  == 0) lednr = 4;
+  else if (strcmp(topic, "zwolle")     == 0) lednr = 5;
+  else if (strcmp(topic, "amersfoort") == 0) lednr = 6;
+  else if (strcmp(topic, "arnhem")     == 0) lednr = 7;
+  else if (strcmp(topic, "wageningen") == 0) lednr = 8;
+  else if (strcmp(topic, "eindhoven")  == 0) lednr = 9;
+  // 10 is niet aangesloten
+  else if (strcmp(topic, "enschede")   == 0) lednr = 11;
+  else if (strcmp(topic, "nijmegen")   == 0) lednr = 12;
+  else if (strcmp(topic, "venlo")      == 0) lednr = 13;
+  else if (strcmp(topic, "heerlen")    == 0) lednr = 14;
+  
+  if (lednr == -1) return;
+
+  leds[lednr]      = length == 3 || length == 6;   // "red"   || "yellow"
+  leds[15 + lednr] = length == 5 || length == 6;   // "green" || "yellow"
+}
+
+
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT server");
+    String clientId = "hoera10jaar-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe("hoera10jaar/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      delay(5000);
     }
-    digitalWrite(col[c], LOW);
   }
 }
 
+void matrix() {
+  for (int c = 0; c < sizeof(col); c++) {
+    for (int r = 0; r < sizeof(row); r++) {
+      digitalWrite(row[r], leds[c * 5 + r] );
+    }
+    digitalWrite(col[c], HIGH);
+    delayMicroseconds(c >= 3 ? brightness*5 : brightness*10);
+    digitalWrite(col[c], LOW);
+    delayMicroseconds(c >= 3 ? 10 : 5);
+  }
+  
+  /*  
+  for (int x = 0; x < 30; x++) {
+    digitalWrite(col[x / 5], leds[x]);
+    digitalWrite(row[x % 5], leds[x]);
+    //delay(1);
+    delayMicroseconds(x >= 15 ? 15 : 20);
+    digitalWrite(col[x / 5], LOW);
+    digitalWrite(row[x % 5], LOW);
+  }
+  */
+}
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("meh");
-  pinMode(p, OUTPUT);
+  Serial.println("o hai");
   for (int r = 0; r < sizeof(row); r++) pinMode(row[r], OUTPUT);  
   for (int c = 0; c < sizeof(col); c++) pinMode(col[c], OUTPUT);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   static int i = 0;
-  Serial.println(i++);
+  if (!client.connected()) reconnect();
   
-  testmatrix(400);
-  unsigned long end = millis() + 1000;
-  while (millis() < end) testmatrix(0);
-    
+  if (i++ % 40 == 0) client.loop();
+  
+  matrix();
 }
