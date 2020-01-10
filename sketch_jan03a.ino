0a4f9cdbf9a0cbb1e1f72a4a48a6a6eb29ae00d6
Juerd Waalboer <juerd@tnx.nl>
2020-01-10 08:12:19 +0100

    Refactor

--- sketch_jan03a.ino
+++ sketch_jan03a.ino
@@ -1,29 +1,27 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <ArduinoOTA.h>
-#include <WiFi.h>
+#include <SPIFFS.h>
 #include <PubSubClient.h>
-#include <esp_task_wdt.h>
-#include <WiFi.h>
 #include <WebServer.h>
 #include <HTTP_Method.h>
-
-#include <SPIFFS.h>
+#include <esp_task_wdt.h>
 
 #define Sprintf(f, ...) ({ char* s; asprintf(&s, f, __VA_ARGS__); String r = s; free(s); r; })
 
-const char* mqtt_server = "hoera10jaar.revspace.nl";
+const char*  mqtt_server = "hoera10jaar.revspace.nl";
+String       my_hostname = "decennium-";
+const byte   row[] = { 27, 26, 32, 33, 25 };  // +
+const byte   col[] = { /* rood */ 4, 17, 18, /* groen */ 16, 5, 19 };  // n fet
+const int    button = 0;
+int          leds[30];
+float        brightness = .3;
 
-String me = "decennium-";
+WiFiClient   espClient;
+PubSubClient mqtt(espClient);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+//////// LED matrix
 
-uint8_t row[] = { 27, 26, 32, 33, 25 };    // +
-uint8_t col[] = { /* rood */ 4, 17, 18, /* groen */ 16, 5, 19 };  // n fet
-int leds[30];
-float brightness = .3;
-
-const int button = 0;
 void matrix() {
   for (int c = 0; c < sizeof(col); c++) {
     for (int r = 0; r < sizeof(row); r++) {
@@ -37,23 +35,14 @@ void matrix() {
     } else {
       delayMicroseconds(c >= 3 ? 10 : 5);
     }
-  }  
-  /*  
-  for (int x = 0; x < 30; x++) {
-    digitalWrite(col[x / 5], leds[x]);
-    digitalWrite(row[x % 5], leds[x]);
-    //delay(1);
-    delayMicroseconds(x >= 15 ? 15 : 20);
-    digitalWrite(col[x / 5], LOW);
-    digitalWrite(row[x % 5], LOW);
   }
-  */
 }
 
 void matrixdelay(int ms) {
   unsigned long end = millis() + ms;
   while(millis() < end) matrix();
 }
+
 void all(int ms, bool red, bool green, float b = 1) {
   float oldb = brightness;
   brightness = b;
@@ -63,69 +52,89 @@ void all(int ms, bool red, bool green, float b = 1) {
   brightness = oldb;
 }
 
+//////// Over-The-Air update
 
+void setup_ota() {
+  String ota = pwgen();
+  Serial.printf("OTA password is %s\n", ota.c_str());
 
-void setup_wifi() {
-  String ssid = SPIFFS.open("/wifi-ssid", "r").readString();
-  String pw = SPIFFS.open("/wifi-password", "r").readString();
-  if (ssid.length() == 0) {
-    Serial.println("First contact!\n");
-    setup_wifi_portal();
-  }
-  Serial.printf("Connecting to %s\n", ssid.c_str());
-  WiFi.begin(ssid.c_str(), pw.c_str());
-  setup_ota();
-  wait_wifi();
+  ArduinoOTA.setHostname(my_hostname.c_str());
+  ArduinoOTA.setPassword(ota.c_str());
+  ArduinoOTA.onStart([]() {
+    all(1, true, true);
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    float p = (float) progress / total;
+    float maxled = p * 30.0;
+    for (int i = 0; i < maxled; i++) {
+      leds[i] = 0;
+    }
+    matrixdelay(2);
+    esp_task_wdt_reset();
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    all(5000, true, false);
+  });
+  ArduinoOTA.onEnd([]() {
+    all(2000, false, true);
+  });
+
+  ArduinoOTA.begin();
 }
 
-void wait_wifi() {
-  int attempts = 0;
-  String r = SPIFFS.open("/wifi-retry", "r").readString();
-  bool retry = r.length();
-  while (WiFi.status() != WL_CONNECTED) {
-    if (attempts++ > 5) {
-      all(500, true, false, .1);
-      check_button();
-      all(500, false, false);
-      check_button();
-      esp_task_wdt_reset();
-    } else {
-      delay(100);
+void check_button() {
+  if (digitalRead(button) == HIGH) return;
+  unsigned long target = millis() + 1000;
+  while (digitalRead(button) == LOW) {
+    if (millis() > target) setup_wifi_portal();
+  }
+}
+
+//////// end-user configurable
+
+String pwgen() {
+  const char* filename   = "/ota-password";
+  const char* passchars  = "ABCEFGHJKLMNPRSTUXYZabcdefhkmnorstvxz23456789-#@%^<>";
+
+  File pwfile = SPIFFS.open(filename, "r");
+  String password = pwfile.readString();
+  pwfile.close();
+
+  if (password.length() == 0) {
+    for (int i = 0; i < 16; i++) {
+       password.concat( passchars[random(strlen(passchars))] );
     }
-    if (attempts > 30 && !retry) {
-      Serial.println("Giving up. Starting config portal.");
-      setup_wifi_portal();
-    }
-    Serial.println(attempts);
+    File pwfile = SPIFFS.open(filename, "w");
+    pwfile.print(password);
+    pwfile.close();
   }
 
-  randomSeed(micros());  // ?
-  Serial.printf("\nIP address: %s\n", WiFi.localIP().toString().c_str());
+  return password;
 }
 
 void setup_wifi_portal() {
   static WebServer http(80);  
   String wpa = SPIFFS.open("/wifi-portal-wpa", "r").readString();
   String ota = pwgen();
-  
+
   if (wpa.length() && ota.length()) {
-    WiFi.softAP(me.c_str(), ota.c_str());
+    WiFi.softAP(my_hostname.c_str(), ota.c_str());
   } else {
-    WiFi.softAP(me.c_str());
+    WiFi.softAP(my_hostname.c_str());
   }
   delay(500);
   setup_ota();
   uint32_t _ip = WiFi.softAPIP();
-  
+
   const String myip = Sprintf("%d.%d.%d.%d", (_ip & 0xff), (_ip >> 8 & 0xff), (_ip >> 16 & 0xff), (_ip >> 24));
   Serial.println(myip);
-  
+
   http.on("/", HTTP_GET, []() {
     String ota = pwgen();
-    
+
     int n = WiFi.scanNetworks();
-    String html = "<!DOCTYPE html>\n<meta charset=UTF-8><title>" + me + "</title>"
-    + "<form action=/restart method=post>Hi, I am " + me + ".<p>Currently configured SSID: %<br><input type=submit value=restart></form>"
+    String html = "<!DOCTYPE html>\n<meta charset=UTF-8><title>" + my_hostname + "</title>"
+    + "<form action=/restart method=post>Hi, I am " + my_hostname + ".<p>Currently configured SSID: %<br><input type=submit value=restart></form>"
     + "<hr><h2>Configure</h2><form method=post>SSID: <select name=ssid>";
     String current = SPIFFS.open("/wifi-ssid", "r").readString();
     if (current.length()) {
@@ -202,80 +211,106 @@ void setup_wifi_portal() {
     ArduinoOTA.handle();
     esp_task_wdt_reset();
   }
-  
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+//////// Wifi + mqtt client
+
+void setup_wifi() {
+  String ssid = SPIFFS.open("/wifi-ssid", "r").readString();
+  String pw = SPIFFS.open("/wifi-password", "r").readString();
+  if (ssid.length() == 0) {
+    Serial.println("First contact!\n");
+    setup_wifi_portal();
+  }
+  Serial.printf("Connecting to %s\n", ssid.c_str());
+  WiFi.begin(ssid.c_str(), pw.c_str());
+  setup_ota();
+  wait_wifi();
+}
+
+void wait_wifi() {
+  int attempts = 0;
+  String r = SPIFFS.open("/wifi-retry", "r").readString();
+  bool retry = r.length();
+  while (WiFi.status() != WL_CONNECTED) {
+    if (attempts++ > 5) {
+      all(500, true, false, .1);
+      check_button();
+      all(500, false, false);
+      check_button();
+      esp_task_wdt_reset();
+    } else {
+      delay(100);
+    }
+    if (attempts > 30 && !retry) {
+      Serial.println("Giving up. Starting config portal.");
+      setup_wifi_portal();
+    }
+    Serial.println(attempts);
+  }
+
+  randomSeed(micros());  // ?
+  Serial.printf("\nIP address: %s\n", WiFi.localIP().toString().c_str());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  String t = topic;
+  t.replace("hoera10jaar/", "");
+
   int lednr = -1;
-
-  while (*topic != '/') topic++;
-  topic++;
-
-  if (     strcmp(topic, "leeuwarden") == 0) lednr = 0;
-  else if (strcmp(topic, "amsterdam")  == 0) lednr = 1;
-  else if (strcmp(topic, "utrecht")    == 0) lednr = 2;
-  else if (strcmp(topic, "denhaag")    == 0) lednr = 3;
-  else if (strcmp(topic, "rotterdam")  == 0) lednr = 4;
-  else if (strcmp(topic, "zwolle")     == 0) lednr = 5;
-  else if (strcmp(topic, "amersfoort") == 0) lednr = 6;
-  else if (strcmp(topic, "arnhem")     == 0) lednr = 7;
-  else if (strcmp(topic, "wageningen") == 0) lednr = 8;
-  else if (strcmp(topic, "eindhoven")  == 0) lednr = 9;
+  if (     t == "leeuwarden") lednr = 0;
+  else if (t == "amsterdam")  lednr = 1;
+  else if (t == "utrecht")    lednr = 2;
+  else if (t == "denhaag")    lednr = 3;
+  else if (t == "rotterdam")  lednr = 4;
+  else if (t == "zwolle")     lednr = 5;
+  else if (t == "amersfoort") lednr = 6;
+  else if (t == "arnhem")     lednr = 7;
+  else if (t == "wageningen") lednr = 8;
+  else if (t == "eindhoven")  lednr = 9;
   // 10 is niet aangesloten
-  else if (strcmp(topic, "enschede")   == 0) lednr = 11;
-  else if (strcmp(topic, "nijmegen")   == 0) lednr = 12;
-  else if (strcmp(topic, "venlo")      == 0) lednr = 13;
-  else if (strcmp(topic, "heerlen")    == 0) lednr = 14;
+  else if (t == "enschede")   lednr = 11;
+  else if (t == "nijmegen")   lednr = 12;
+  else if (t == "venlo")      lednr = 13;
+  else if (t == "heerlen")    lednr = 14;
   
   if (lednr == -1) return;
 
-  leds[lednr]      = length == 3 || length == 6;   // "red"   || "yellow"
-  leds[15 + lednr] = length == 5 || length == 6;   // "green" || "yellow"
+  char* m = (char*) message;
+  if (strncmp(m, "red", length) == 0) {
+    leds[lednr]      = 1;
+    leds[lednr + 15] = 0;
+  } else if (strncmp(m, "green", length) == 0) {
+    leds[lednr]      = 0;
+    leds[lednr + 15] = 1;
+  } else if (!length) {
+    leds[lednr]      = 0;
+    leds[lednr + 15] = 0;
+  } else {  // "yellow" waarschijnlijk
+    leds[lednr]      = 1;
+    leds[lednr + 15] = 1;
+  }
 }
 
-void reconnect() {
-  while (!client.connected()) {
+void reconnect_mqtt() {
+  // Als de wifi weg is, blijft dit hangen en grijpt de watchdog in.
+  // WiFi.status() blijft echter op WL_CONNECTED dus slimmere afhandeling is lastig.
+
+  while (!mqtt.connected()) {
     Serial.print("Connecting to MQTT server");
 
-    if (client.connect(me.c_str())) {
+    if (mqtt.connect(my_hostname.c_str())) {
       Serial.println("connected");
-      client.subscribe("hoera10jaar/+");
+      mqtt.subscribe("hoera10jaar/+");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqtt.state());
       all(5000, true, true, .1);
     }
   }
 }
 
-
-String pwgen() {
-  const char* filename   = "/ota-password";
-  const char* passchars  = "ABCEFGHJKLMNPRSTUXYZabcdefhkmnorstvxz23456789-#@%^<>";
-  
-  File pwfile = SPIFFS.open(filename, "r");
-  String password = pwfile.readString();
-  pwfile.close();
-
-  if (password.length() == 0) {
-    for (int i = 0; i < 16; i++) {
-       password.concat( passchars[random(strlen(passchars))] );
-    }
-    File pwfile = SPIFFS.open(filename, "w");
-    pwfile.print(password);
-    pwfile.close();
-  }
-  
-  return password;
-}
-
-void check_button() {
-  if (digitalRead(button) == HIGH) return;
-  unsigned long target = millis() + 1000;
-  while (digitalRead(button) == LOW) {
-    if (millis() > target) setup_wifi_portal();
-  }
-}
+//////// Main
 
 void setup() {
   for (int r = 0; r < sizeof(row); r++) pinMode(row[r], OUTPUT);  
@@ -283,11 +318,11 @@ void setup() {
   all(50, false, true, .1);
   all(0, false, false, .1);
   pinMode(button, INPUT);
-  
+
   Serial.begin(115200);
   Serial.println("o hai");
-  me += Sprintf("%12" PRIx64, ESP.getEfuseMac());
-  Serial.println(me);
+  my_hostname += Sprintf("%12" PRIx64, ESP.getEfuseMac());
+  Serial.println(my_hostname);
 
   esp_task_wdt_init(30 /* seconds */, true);
   esp_err_t err = esp_task_wdt_add(NULL);
@@ -296,55 +331,19 @@ void setup() {
   SPIFFS.begin(true);
 
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  mqtt.setServer(mqtt_server, 1883);
+  mqtt.setCallback(callback);
 }
 
-void setup_ota() {
-  
-  String ota = pwgen();
-  Serial.printf("OTA password is %s\n", ota.c_str());
-  
-  ArduinoOTA.setHostname(me.c_str());
-  ArduinoOTA.setPassword(ota.c_str());
-  ArduinoOTA.onStart([]() {
-    all(1, true, true);
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    float p = (float) progress / total;
-    float maxled = p * 30.0;
-    for (int i = 0; i < maxled; i++) {
-      leds[i] = 0;
-    }
-    matrixdelay(2);
-    esp_task_wdt_reset();
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    all(5000, true, false);
-  });
-  ArduinoOTA.onEnd([]() {
-    all(2000, false, true);
-  });
-  
-  ArduinoOTA.begin();
-}
 
 void loop() {
-  static int i = 0;
-  if (WiFi.status() != WL_CONNECTED) {
-    // Werkt niet, status blijft WL_CONNECTED ook als AP weg is...
-    // Dus hij blijft straks in de mqtt reconnect() hangen. Watchdog grijpt in.
-    Serial.println("Reconnecting wifi...\n");
-    WiFi.disconnect();
-    setup_wifi();
-  }
-  
-  if (WiFi.status() == WL_CONNECTED && !client.connected()) reconnect();
-  
-  if (i++ % 40 == 0) client.loop();
-  
-  matrix();
+  if (!mqtt.connected()) reconnect_mqtt();
 
+  // Traag, dus niet te vaak doen ivm matrix refresh rate
+  static unsigned int i = 0;
+  if (i++ % 40 == 0) mqtt.loop();
+
+  matrix();
   ArduinoOTA.handle();
   esp_task_wdt_reset();
   check_button();
